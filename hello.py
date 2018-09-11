464731871be45fa5286b45bc10b53155639120da
Henrique Fonseca <henriquewho@live.com>
2018-09-11 17:59:36 -0300

    aaa

--- hello.py
+++ hello.py
@@ -1,14 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, g, render_template
 import sqlite3
 
 app = Flask(__name__)
+app.database = "sample.db"
+
 
 
 @app.route("/")
 def index():
-    return "<h1>Teste Geraldo1!</hi>"
+	g.db = connect_db()
+	cur = g.db.execute('select * from posts')
+	posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
+	g.db.close()
+    	return render_template('index.html', posts=posts)
+
+def connect_db():
+	return sqlite3.connect(app.database)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    	port = int(os.environ.get("PORT", 5000))
+    	app.run(host='0.0.0.0', port=port)
